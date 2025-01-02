a23f334ebb45b0521f362fbfb0cd6d6539c8d82d
Dzhafar Nasirov <xjfxnsx@gmail.com>
2025-01-02 22:56:27 +0400

    modified main.tsx

--- weather-app/src/main.tsx
+++ weather-app/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Tolgee, DevTools, TolgeeProvider, FormatSimple } from "@tolgee/react";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const tolgee = Tolgee().use(DevTools()).use(FormatSimple()).init({
+  language: "en",
+
+  // for development
+  apiUrl: process.env.VITE_APP_TOLGEE_API_URL,
+  apiKey: process.env.VITE_APP_TOLGEE_API_KEY,
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <TolgeeProvider tolgee={tolgee} fallback="Loading...">
+      <App />
+    </TolgeeProvider>
+  </StrictMode>
+);
